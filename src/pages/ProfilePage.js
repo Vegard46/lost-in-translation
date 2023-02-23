42694568b4f4ed3c1ff5addc43f7da3da909b26d
vegardandersson <veg_ander@hotmail.com>
2023-02-23 13:52:14 +0100

    some styling, added final server functionality

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -9,6 +9,7 @@ function ProfilePage() {
   const navigate = useNavigate();
 
   const apiUrl = "https://noroff-api-production-156b.up.railway.app";
+  const apiKey = "1XMN2BaYYgxgu1sRhzWU0DydzNroZmnXNbzGNifZjiCINlNYHTKCNXSMrzhIDHTj";
 
   useEffect(() => {
     fetchTranslations();
@@ -19,14 +20,40 @@ function ProfilePage() {
   })
 
   function fetchTranslations(){
-    fetch(apiUrl + "/translations")
+    fetch(apiUrl + "/translations?username=" + localStorage.getItem("user"))
+    .then(response => response.json())
+    .then(user => {
+      fetch(apiUrl + "/translations/" + user[0].id)
       .then(response => response.json())
       .then(result => {
-        let transl = result[0].translations;
+        console.log(result);
+        let transl = result.translations;
         transl.length = transl.length > 10 ? 10 : transl.length;
         setTranslations(transl)
       })
       .catch(error => console.error(error));
+    })
+  }
+
+  const clearTranslations = () => {
+    if(!translations){return;}
+    fetch(apiUrl + '/translations?username=' + localStorage.getItem("user"))
+    .then(response => response.json())
+    .then(user => {
+      fetch(apiUrl + '/translations/' + user[0].id, {
+        method: 'PATCH',
+        headers: {
+          'x-api-key': apiKey,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          translations: []
+        })
+      })
+      .then(response => response.json())
+      .then(result => setTranslations([]))
+      .catch(error => console.error(error));
+    })
   }
 
   return (
@@ -39,7 +66,7 @@ function ProfilePage() {
             : <li id='first-list-item' key={index} style={{opacity: (100-index*6)/100}}>{e}</li>
         })}
       </ul>
-      <LtDeleteButton text='Clear'/>
+      <LtDeleteButton text='Clear' onClick={clearTranslations}/>
     </div>
   );
 }
--- src/pages/TranslationPage.js
+++ src/pages/TranslationPage.js
@@ -12,13 +12,40 @@ function TranslationPage() {
   const [images, setImages] = useState([]);
   const navigate = useNavigate();
 
-  const apiUrl = "https://wandering-life-jacket-goat.cyclic.app";
+  const apiUrl = "https://noroff-api-production-156b.up.railway.app";
+  const apiKey = "1XMN2BaYYgxgu1sRhzWU0DydzNroZmnXNbzGNifZjiCINlNYHTKCNXSMrzhIDHTj";
 
   useEffect(() => {
     if(!localStorage.getItem("user")){navigate('/')}
   })
 
-  const fetchImages = () => {
+  const processTranslation = () => {
+    if(!translation){return;}
+    fetchImages();
+    fetch(apiUrl + '/translations?username=' + localStorage.getItem("user"))
+    .then(response => response.json())
+    .then(user => {
+
+      let transl = user[0].translations;
+      transl.push(translation)
+
+      fetch(apiUrl + '/translations/' + user[0].id, {
+        method: 'PATCH',
+        headers: {
+          'x-api-key': apiKey,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          translations: transl
+        })
+      })
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.error(error));
+    })
+  }
+
+  function fetchImages(){
     let words = translation.toLowerCase().trim().split(" ");
     let wordsImages = [];
     words.map(word => {
@@ -35,13 +62,13 @@ function TranslationPage() {
 
   return (
       <div className="translation-page">
-        <LtSubmitWidget text='Translate a phrase...' onClick={fetchImages} onChange={setTranslation} value={translation}/>
+        <LtSubmitWidget text='Translate a phrase...' onClick={processTranslation} onChange={setTranslation} value={translation}/>
         <div className='translation-container'>
             {images.map((e, index) => {
               return index < images.length-1 
               ? <div className='row-container'>
                   <LtImageRow images={e}/>
-                  <i class="fa fa-plus image-add" aria-hidden="true"/>
+                  <i className="fa fa-plus image-add" aria-hidden="true"/>
                 </div> 
               : <div className='row-container'>
                   <LtImageRow images={e}/>
