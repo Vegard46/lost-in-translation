03b5a2831270d979b0600295ef0dfb41f2b4fb8b
Vegard46 <veg_ander@hotmail.com>
2023-02-24 14:25:58 +0100

    fixed uncontrolled input warning making it controlled

--- src/context/TranslationProvider.js
+++ src/context/TranslationProvider.js
@@ -4,7 +4,7 @@ export const TranslationContext = createContext();
 
 function TranslationProvidor({children}) {
 
-    const [translation, setTranslation] = useState();
+    const [translation, setTranslation] = useState("");
 
     return (
         <TranslationContext.Provider value={[translation, setTranslation]}>
